8729dbb70d6feb814e8ce8359f9547d8d85444b1
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-03-02 19:13:55 +0100

    Interpretation of images

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const { connectToDB, saveMessage, readMessages } = require("./dbConnect");
 // Import the OpenAI SDK to be able to send queries to the OpenAI API.
 const OpenAI = require("openai");
 // Import mongoose for closing connection to DB;
-const { default: mongoose } = require("mongoose");
+const { default: mongoose, model } = require("mongoose");
 
 //Import library for conwering message to tokens;
 const { encoding_for_model } = require("@dqbd/tiktoken");
+const { message } = require("telegraf/filters");
 
 //Maximum size of one request. History and qestion to AI API;
 const maxTokensInRequest = 3400;
-const model = "gpt-4o-mini";
+const defaultModel = "gpt-4o-mini";
 
 if (envResult.error) {
   console.error("Error while loading .env file", envResult.error);
@@ -40,6 +41,30 @@ connectToDB();
 
 const openai = new OpenAI({ apiKey: openaiKey });
 
+// async function imageInterpreter() {
+//   const response = await openai.chat.completions.create({
+//     model: defaultModel,
+//     messages: [
+//       {
+//         role: "user",
+//         content: [
+//           { type: "text", text: "Whats's in this image?" },
+//           {
+//             type: "image_url",
+//             image_url: {
+//               url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//     store: true,
+//   });
+//   console.log(response.choices[0]);
+// }
+
+// imageInterpreter();
+
 function formatHistoryToString(history) {
   let formatedHistory = history
     .map((item) => `${item.role} : ${item.content}`)
@@ -47,39 +72,85 @@ function formatHistoryToString(history) {
   return formatedHistory;
 }
 
+function validateIsFileImage(fileId) {
+  const fileFormat = fileId.file_path.split(".").pop();
+
+  if (!["jpg", "jpeg", "png"].includes(fileFormat)) {
+    ctx.reply("Błędny format pliku. Umiem obsłurzyć tylko pliki JPG lub PNG");
+    return false;
+  }
+  return true;
+}
+
+async function processImagesAI(fileUrl, instructions) {
+  try {
+    const response = await openai.chat.completions.create({
+      model: defaultModel,
+      messages: [
+        {
+          role: "user",
+          content: [
+            { type: "text", text: instructions },
+            { type: "image_url", image_url: { url: fileUrl } },
+          ],
+        },
+      ],
+      temperature: 0.3,
+      store: true,
+    });
+
+    return response.choices[0].message.content;
+  } catch (error) {
+    console.error("Błąd przetwarzania obrazu przez AI: ", error);
+  }
+}
+
 // Function for communicating with OpenAI
-async function askChatText(text, sesionID) {
-  const historyMessages = await readMessages(sesionID);
+// query structure {text: "exaple text", imageUrl: "url string to file send from telegram"}
+async function askChat(query, sessionID) {
+  if (!query) return new Error("Brak zapytania 'query'");
+  if (!sessionID) return new Error("Brak sessionID");
+
+  let text = query.text ?? null;
+  let imageUrl = query.imageUrl ?? null;
+
+  const historyMessages = await readMessages(sessionID);
 
   let replyMessage = "";
+  const tokenizer = encoding_for_model(defaultModel);
+  let historySizeInTokens = tokenizer.encode(
+    formatHistoryToString(historyMessages)
+  ).length;
 
-  if (typeof text === "string" && text.length > 0) {
-    const tokenizer = encoding_for_model(model);
-    let historySizeInTokens = tokenizer.encode(
-      formatHistoryToString(historyMessages)
-    ).length;
+  let messageInTokens = tokenizer.encode(text).length;
 
-    let messageInTokens = tokenizer.encode(text).length;
+  console.log(
+    "Ilość tokenów przekazanych historii: ",
+    historySizeInTokens + messageInTokens
+  );
 
-    if (historySizeInTokens + messageInTokens >= maxTokensInRequest) {
-      while (historySizeInTokens + messageInTokens >= maxTokensInRequest) {
-        historyMessages.splice(0, 1);
-        historySizeInTokens = tokenizer.encode(
-          formatHistoryToString(historyMessages)
-        ).length;
-        messageInTokens = tokenizer.encode(text).length;
-      }
-      console.log("Token limit reach. Redusing history in request.");
+  if (historySizeInTokens + messageInTokens >= maxTokensInRequest) {
+    while (historySizeInTokens + messageInTokens >= maxTokensInRequest) {
+      historyMessages.splice(0, 1);
+      historySizeInTokens = tokenizer.encode(
+        formatHistoryToString(historyMessages)
+      ).length;
+      messageInTokens = tokenizer.encode(text).length;
     }
-    tokenizer.free();
+    console.log("Token limit reach. Redusing history in request.");
+  }
+  tokenizer.free();
 
+  // text query usage
+  if (text && typeof text === "string" && text.length > 0) {
     try {
       const completion = await openai.chat.completions.create({
-        model: model,
+        model: defaultModel,
         messages: [
           {
             role: "system",
-            content: "You are chatBot named GrzybekAIbot. Use Polish language",
+            content:
+              "You are chatBot named GrzybekAIbot. Use Polish language. Nie używaj frazy 'Na podstawie wcześniejszych informacji' oraz 'z opisu kótry podałeś';",
           },
           ...historyMessages,
           { role: "user", content: text },
@@ -92,6 +163,13 @@ async function askChatText(text, sesionID) {
       console.error("OpenAI response error: ", error);
       throw error;
     }
+  }
+
+  // image query usage
+  if (imageUrl && imageUrl.length > 0) {
+    console.log("przetwarzam obraz");
+    const requestAction = query.text;
+    replyMessage = await processImagesAI(imageUrl, requestAction);
   }
   return replyMessage;
 }
@@ -112,12 +190,13 @@ bot.command("stop", (ctx) => {
 });
 
 bot.on("text", async (ctx) => {
+  console.log("text mode");
   const receivedMessage = ctx.message.text;
   const sessionId = ctx.from.id.toString();
   try {
-    const replayText = await askChatText(receivedMessage, sessionId);
-    const assistantMessage = { role: "assistant", content: replayText.content };
+    const replayText = await askChat({ text: receivedMessage }, sessionId);
     const sentedMessage = { role: "user", content: receivedMessage };
+    const assistantMessage = { role: "assistant", content: replayText.content };
 
     await saveMessage(sessionId, sentedMessage.role, sentedMessage.content);
 
@@ -133,6 +212,92 @@ bot.on("text", async (ctx) => {
     ctx.reply(
       "Przepraszam, wystąpił błąd podczas przetwarzania Twojej wiadomości. Spróbuj ponownie później."
     );
+  }
+});
+
+bot.on("photo", async (ctx) => {
+  console.log("Photo mode");
+
+  const sessionId = ctx.from.id.toString();
+
+  try {
+    const recivedPhoto = ctx.message.photo;
+    const photoId = recivedPhoto[recivedPhoto.length - 1].file_id;
+
+    const fileId = await ctx.telegram.getFile(photoId);
+    const fileURL = `https://api.telegram.org/file/bot${botApiKey}/${fileId.file_path}`;
+
+    if (validateIsFileImage(fileId)) {
+      saveMessage(sessionId, "user", fileURL);
+    }
+
+    await ctx.reply("Odebrałem obraz.\n Co chcesz abym z nim zrobił ?", {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "📄 Opisz obraz", callback_data: "describe" }],
+          [{ text: "📊 Analizuj dokumnet", callback_data: "analyze_doc" }],
+        ],
+      },
+    });
+  } catch (error) {
+    console.error("Bład podczas pobierania obrazu: ", error);
+    ctx.reply("Błąd przetwarzania obrazu");
+  }
+});
+
+bot.on("callback_query", async (ctx) => {
+  try {
+    const selectedAction = ctx.callbackQuery.data;
+    const sessionID = ctx.from.id.toString();
+    const historyMessages = await readMessages(sessionID);
+    const sendedImageUrl = historyMessages[historyMessages.length - 1].content;
+    if (!sendedImageUrl) {
+      ctx.reply("Nie mogę znaleść obrazu w historii rozmowy..");
+      return;
+    }
+
+    switch (selectedAction) {
+      case "describe": {
+        const reviceDescription = await askChat(
+          {
+            text: "Co jest na tym obrazku",
+            imageUrl: sendedImageUrl,
+          },
+          sessionID
+        );
+
+        ctx.reply(reviceDescription);
+
+        await saveMessage(sessionID, "user", "Co jest na tym obrazku");
+        await saveMessage(
+          sessionID,
+          "assistant",
+          `Zapamiętałem, że ${reviceDescription}`
+        );
+        break;
+      }
+      case "analyze_doc":
+        const reviceDescription = await askChat(
+          {
+            text: "Przeanalizuj dokument.Jeśli nie wykryjesz tekstu, poinformuj w przyjazny sposób",
+            imageUrl: sendedImageUrl,
+          },
+          sessionID
+        );
+        await saveMessage(
+          sessionID,
+          "user",
+          "Przeanalizuj dokument.Jeśli nie wykryjesz tekstu, poinformuj w przyjazny sposób"
+        );
+        await saveMessage(sessionID, "assistant", reviceDescription);
+        ctx.reply(reviceDescription);
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error("Błąd obsługi callback_query: ", error);
+    ctx.reply("Wystąpił błąd.Spróbuj ponownie.");
   }
 });
 
