b6a32c40928df9cd3fa2a07d760a37b72afcd9a9
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-03-17 08:32:47 +0100

    merge

---
+++ src/services/tavily/TavilyService.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+
+module.exports = class TavilyService {
+  constructor(tavilyApiKey) {
+    if (!tavilyApiKey) {
+      console.error("Brak wymaganych kluczy API: Tavily w pliku .env");
+      process.exit(1);
+    }
+    this.tavilyApiKey = tavilyApiKey;
+    this.baseUrl = "https://api.tavily.com";
+  }
+
+  async search(query) {
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/search`,
+        {
+          query: query,
+          topic: "general",
+          search_depth: "basic",
+          max_results: 4,
+          time_range: null,
+          days: 3,
+          include_answer: true,
+          include_raw_content: false,
+          include_images: false,
+          include_image_descriptions: false,
+          include_domains: [],
+          exclude_domains: [],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.tavilyApiKey}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return {
+        success: true,
+        results: response.data,
+      };
+    } catch (error) {
+      console.error(
+        "Nie udało się pobrać wyników wyszukiwania z Tavily. Spróbuj ponownie później. Szczegóły błędu:",
+        error
+      );
+      return { success: false, error: error.message };
+    }
+  }
+};
---
+++ src/utils/formatters/formatWebSearchResponse.js
@@ -0,0 +1,24 @@
+function formatWebSearchResponse(response) {
+  try {
+    if (
+      typeof response === "object" &&
+      typeof response.answer === "string" &&
+      typeof response.responses === "object"
+    ) {
+      const { answer, responses } = response;
+      const higSocereResponses = responses
+        .map((res) => {
+          if (res.score > 0.65) {
+            return `content: ${res.content}, url: ${res.url}`;
+          }
+        })
+        .filter((res) => res != undefined);
+      const formattedString = `Odpowiedź główna: ${answer} żródła: ${higSocereResponses.join()}`;
+      return { success: response.success, formattedString };
+    }
+  } catch (error) {
+    console.error("Błędny format odpowiedzi z usługi web_Search: " + error);
+  }
+}
+
+module.exports = formatWebSearchResponse;
